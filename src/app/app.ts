64a692c60f00b63f9137698eee80988047580f9c
darlan <dmarangoni.dev@gmail.com>
2026-02-03 07:34:23 -0300

    learn: Composing Components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+    selector: 'app-user',
+    template: `Username: {{ username }}`,
+  }
+)
+export class User {
+  username = 'Darlan';
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, User],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('personal-finance-app');
+
+  city = 'San Francisco';
 }
