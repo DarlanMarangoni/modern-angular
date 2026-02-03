c25578913663a9d10a289e635cead6e87d220e71
darlan <dmarangoni.dev@gmail.com>
2026-02-03 18:55:30 -0300

    learn: Optimizing images

--- README.md
+++ README.md
@@ -74,3 +74,5 @@ For more information on using the Angular CLI, including detailed command refere
 7- https://angular.dev/tutorials/learn-angular/9-output
 
 8 - https://angular.dev/tutorials/learn-angular/10-deferrable-views
+
+9 - https://angular.dev/tutorials/learn-angular/11-optimizing-images
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,78 +1,40 @@
 import {Component} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
-  selector: 'comments',
+  selector: 'app-user',
   template: `
+    <p>Username: {{ username }}</p>
+    <p>Preferred Framework:</p>
     <ul>
-      <li>Building for the web is fantastic!</li>
-      <li>The new template syntax is great</li>
-      <li>I agree with the other comments!</li>
+      <li>
+        Static Image:
+        <img ngSrc="hommer.jpeg" alt="Angular logo" width="225" height="225"/>
+      </li>
+      <li>
+        Dynamic Image:
+        <img [ngSrc]="logoUrl" [alt]="logoAlt" width="32" height="32"/>
+      </li>
     </ul>
   `,
+  imports: [NgOptimizedImage],
 })
-export class Comments {}
+export class User {
+  logoUrl = 'hommer.jpeg';
+  logoAlt = 'Angular logo';
+  username = 'youngTech';
+}
+
 
 
 @Component({
   selector: 'app-root',
   template: `
-    <div>
-      <h1>How I feel about Angular</h1>
-      <article>
-        <p>
-          Angular is my favorite framework, and this is why. Angular has the coolest deferrable view
-          feature that makes defer loading content the easiest and most ergonomic it could possibly
-          be. The Angular community is also filled with amazing contributors and experts that create
-          excellent content. The community is welcoming and friendly, and it really is the best
-          community out there.
-        </p>
-        <p>
-          I can't express enough how much I enjoy working with Angular. It offers the best developer
-          experience I've ever had. I love that the Angular team puts their developers first and
-          takes care to make us very happy. They genuinely want Angular to be the best framework it
-          can be, and they're doing such an amazing job at it, too. This statement comes from my
-          heart and is not at all copied and pasted. In fact, I think I'll say these exact same
-          things again a few times.
-        </p>
-        <p>
-          Angular is my favorite framework, and this is why. Angular has the coolest deferrable view
-          feature that makes defer loading content the easiest and most ergonomic it could possibly
-          be. The Angular community is also filled with amazing contributors and experts that create
-          excellent content. The community is welcoming and friendly, and it really is the best
-          community out there.
-        </p>
-        <p>
-          I can't express enough how much I enjoy working with Angular. It offers the best developer
-          experience I've ever had. I love that the Angular team puts their developers first and
-          takes care to make us very happy. They genuinely want Angular to be the best framework it
-          can be, and they're doing such an amazing job at it, too. This statement comes from my
-          heart and is not at all copied and pasted. In fact, I think I'll say these exact same
-          things again a few times.
-        </p>
-        <p>
-          Angular is my favorite framework, and this is why. Angular has the coolest deferrable view
-          feature that makes defer loading content the easiest and most ergonomic it could possibly
-          be. The Angular community is also filled with amazing contributors and experts that create
-          excellent content. The community is welcoming and friendly, and it really is the best
-          community out there.
-        </p>
-        <p>
-          I can't express enough how much I enjoy working with Angular. It offers the best developer
-          experience I've ever had. I love that the Angular team puts their developers first and
-          takes care to make us very happy. They genuinely want Angular to be the best framework it
-          can be, and they're doing such an amazing job at it, too. This statement comes from my
-          heart and is not at all copied and pasted.
-        </p>
-      </article>
-      @defer (on viewport) {
-        <comments />
-      } @placeholder {
-        <p>Future comments</p>
-      } @loading (minimum 10s) {
-        <p>Loading comments...</p>
-      }
-    </div>
+    <app-user />
   `,
-  imports: [Comments],
+  imports: [
+    User
+  ],
 })
-export class App {}
+export class App {
+}
