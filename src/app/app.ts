3b1d49e1696103a6e2ce5a1d30b57f194d89b1d0
darlan <dmarangoni.dev@gmail.com>
2026-02-05 10:42:53 -0300

    feat: Adicionando primeng

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
-import {Component, inject} from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [],
+  imports: [
+    RouterOutlet,
+  ]
 })
 export class App {
 
