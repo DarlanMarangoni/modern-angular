ec998aa0166a2ce91a877ccd921e2d7504246208
darlan <dmarangoni.dev@gmail.com>
2026-02-05 10:53:40 -0300

    feat: Adicionando menu bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
 
 
 @Component({
@@ -7,8 +9,31 @@ import {RouterOutlet} from '@angular/router';
   templateUrl: './app.html',
   imports: [
     RouterOutlet,
+    MenubarModule
   ]
 })
-export class App {
+export class App implements OnInit {
+  protected items: MenuItem[] | undefined;
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home'
+      },
+      {
+        label: 'Despesas',
+        icon: 'pi pi-home'
+      },
+      {
+        label: 'Proventos',
+        icon: 'pi pi-home'
+      },
+      {
+        label: 'Investimentos',
+        icon: 'pi pi-home'
+      }
+    ]
+  }
 
 }
