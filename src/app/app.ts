b158a0a6e815109f74675d13cd571c3b85a82e22
darlan <dmarangoni.dev@gmail.com>
2026-02-05 10:36:33 -0300

    feat: Clean

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,30 +1,11 @@
 import {Component, inject} from '@angular/core';
-import {CarService} from './car.service';
-import {CurrencyPipe, DatePipe, DecimalPipe} from '@angular/common';
-import {ReversePipe} from './reverse.pipe';
 
 
 @Component({
   selector: 'app-root',
-  template: `
-    <li>Number with "decimal": {{ num | number: '3.2-2' }}</li>
-    <li>Date with "date": {{ birthday | date: 'medium' }}</li>
-    <li>Currency with "currency": {{ cost | currency }}</li>
-    <li>Reverse pipe on "Darlan": {{ 'Darlan' | reverse }}</li>
-  `,
-  imports: [
-    DecimalPipe,
-    DatePipe,
-    CurrencyPipe,
-    ReversePipe
-  ],
+  templateUrl: './app.html',
+  imports: [],
 })
 export class App {
-
-  carService = inject(CarService);
-
-  num = 1234567.89;
-  birthday = new Date();
-  cost = 1.99;
 
 }
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,19 +1,9 @@
-import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.html',
   styleUrl: './user.scss',
-  imports: [FormsModule],
 })
 export class User {
-
-  username = 'Darlan';
-  favoriteFramework = '';
-
-  showFramework() {
-    alert(this.favoriteFramework);
-  }
-
 }
