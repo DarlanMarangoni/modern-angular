1224b28d2f41e73ffb38fd9122179b0b428008ea
darlan <dmarangoni.dev@gmail.com>
2026-02-11 19:17:19 -0300

    feat: Criando tela de investimentos

--- src/app/pages/investimentos/investimentos.ts
+++ src/app/pages/investimentos/investimentos.ts
@@ -1,12 +1,74 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
+import {MatButton, MatButtonModule} from '@angular/material/button';
+import {
+  MatDatepicker,
+  MatDatepickerInput,
+  MatDatepickerModule,
+  MatDatepickerToggle
+} from '@angular/material/datepicker';
+import {
+  MatError,
+  MatFormField,
+  MatInput,
+  MatInputModule,
+  MatLabel,
+  MatPrefix,
+  MatSuffix
+} from '@angular/material/input';
+import {MatNativeDateModule, MatOption} from '@angular/material/core';
+import {MatSelect, MatSelectModule} from '@angular/material/select';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-investimentos',
-  imports: [],
+  imports: [
+    MatButton,
+    MatDatepicker,
+    MatDatepickerInput,
+    MatDatepickerToggle,
+    MatError,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatOption,
+    MatPrefix,
+    MatSelect,
+    MatSuffix,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
   templateUrl: './investimentos.html',
   styleUrl: './investimentos.scss',
   standalone: true
 })
 export class Investimentos {
+  fb = inject(FormBuilder);
+
+  form = this.fb.nonNullable.group({
+    nome: ['', Validators.required],
+    categoria: ['', Validators.required],
+    descricao: [''],
+    data: [new Date(), Validators.required],
+    valor: [0, [Validators.required]]
+  });
+
+  tipos = signal<String[]>([]);
+
+  ngOnInit(): void {
+    this.tipos = signal(['Acoes', 'Fundo Imobiliario', 'Renda Fixa', 'Previdencia Privada', 'CDB', 'FGTS', 'Tesouro Direto', 'Fundo de Investimento']);
+  }
+
+  salvar() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+  }
 
 }
